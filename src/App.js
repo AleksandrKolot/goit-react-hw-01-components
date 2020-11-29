ccdca3050cefc1281791412ff105aa6d6f756eed
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2020-11-29 22:06:38 +0200

    Statistics

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 // import FriendList from './components/FriendList/FriendList';
 // import friends from './components/data/friends.json';
-import Profile from './components/Profile/Profile';
-import user from './components/data/user.json';
+// import Profile from './components/Profile/Profile';
+// import user from './components/data/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/data/statistical-data.json';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Profile
+      {/* <Profile
         name={user.name}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      />
+      /> */}
+      <Statistics title="Upload stats" stats={statisticalData} />
       {/* <FriendList friends={friends} /> */}
     </div>
   );
