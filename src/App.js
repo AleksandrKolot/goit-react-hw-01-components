e2b7039a9c7e75715680328470655ee444f5128d
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2020-11-29 20:07:53 +0200

    profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import FriendList from './components/FriendList/FriendList';
+// import FriendList from './components/FriendList/FriendList';
+// import friends from './components/data/friends.json';
+import Profile from './components/Profile/Profile';
+import user from './components/data/user.json';
+
 import './App.css';
-import friends from './components/data/friends.json';
 
 function App() {
   return (
     <div className="App">
-      <FriendList friends={friends} />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <FriendList friends={friends} /> */}
     </div>
   );
 }
